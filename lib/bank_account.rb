179d71c2b4bd82eb1da00ee944e84eaf0e93018c
Cat Gomez <catherinedgomez@gmail.com>
2020-03-19 23:20:17 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,9 @@
 class BankAccount
 
 attr_accessor
-attr_reader :bank_accounts, :name
+attr_reader :bank_accounts #, :name
 
-  def initialize(bank_accounts, name)
+  def initialize(bank_accounts)
     @bank_accounts = bank_accounts
     # @name = name
   end
